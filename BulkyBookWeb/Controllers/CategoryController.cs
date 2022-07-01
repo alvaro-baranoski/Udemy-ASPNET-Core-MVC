ca70a1167e496f663e02b840fe04211a095cdfd2
alvaro-baranoski <alvarojosebaranoski01@gmail.com>
2022-07-01 18:25:58 -0300

    Mudanças até aula 30

---
+++ BulkyBookWeb/Controllers/CategoryController.cs
@@ -0,0 +1,22 @@
+﻿using BulkyBookWeb.Data;
+using BulkyBookWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyBookWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Category> objCaterogyList = _db.Categories.ToList();
+            return View(objCaterogyList);
+        }
+    }
+}
