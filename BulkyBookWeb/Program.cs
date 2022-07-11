7563e6f6acb04e6e5d149193def911c2a43eef5f
alvaro-baranoski <alvarojosebaranoski01@gmail.com>
2022-07-11 17:06:04 -0300

    Adiciona roles na base de dados

--- BulkyBookWeb/Program.cs
+++ BulkyBookWeb/Program.cs
@@ -3,20 +3,32 @@ using BulkyBook.DataAccess.Repository;
 using BulkyBook.DataAccess.Repository.IRepository;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using BulkyBook.Utility;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddDefaultIdentity<IdentityUser>()
-    .AddEntityFrameworkStores<ApplicationDbContext>();
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
 builder.Services.AddDbContext<ApplicationDbContext>(options => 
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
-    );
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")
+    ));
+
+// builder.Services.AddDefaultIdentity<IdentityUser>()
+//     .AddEntityFrameworkStores<ApplicationDbContext>();
+
+builder.Services.AddIdentity<IdentityUser, IdentityRole>()
+    .AddDefaultTokenProviders()
+    .AddEntityFrameworkStores<ApplicationDbContext>();
+
+builder.Services.AddSingleton<IEmailSender, EmailSender>();
+
 
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+builder.Services.AddRazorPages();
 
 var app = builder.Build();
 
